190a579b6068431cfc43daffc16f25590e270abf
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2025-01-16 07:17:30 +0330

    ✨ Add basic routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Calendar } from './calendar/calendar.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Calendar],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Calendar } from './calendar/calendar.component';
+import { Appointment } from './appointment/appointment.component';
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        component: Calendar,
+      },
+      {
+        path: 'create-appointment',
+        component: Appointment,
+      },
+    ],
+  },
+];
